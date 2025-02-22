854a2510cccc76eaf24abb7a1295a52c88ef6a69
Пакилев Дмитрий <pakilev08@mail.ru>
2025-02-22 19:59:41 +0800

    Была создана модель для передачи данных от пользователя к модели

--- classification_website/image/models.py
+++ classification_website/image/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+# Create your models here.
+
+
+
+class Image(models.Model):
+    MODEL_CHOICES = [
+        ('rn50', 'ResNet50'),
+        ('v16', 'Vgg16')
+    ]
+
+    image = models.ImageField(blank=False, upload_to='files/')
+
+    model_classification = models.CharField(max_length=60,
+                                            choices=MODEL_CHOICES,
+                                             name='model',
+                                             verbose_name='Модель для классификации картинки'
+                                             )
+
+
