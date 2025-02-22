eace799d218c9c72bc500225952618c62d3d29fc
Пакилев Дмитрий <pakilev08@mail.ru>
2025-02-22 20:14:36 +0800

    Была создан метод во view для передачи формы пользователю

--- classification_website/image/views.py
+++ classification_website/image/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .forms import ResumeForm
 # Create your views here.
+
+def upload_image(request):
+    if request.method == "POST":
+        pass
+    else:
+        form = ResumeForm
+        return render(request, 'file_upload.html', {'form': form})
